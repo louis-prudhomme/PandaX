bc9ffd24ab7b9a024133ef1186f4ee97b4ae215e
PrideTheDaemon <louis.prudhomme@hotmail.com>
2019-11-16 11:51:00 +0100

    Created Maven project with test path

---
+++ src/main/java/fr/efrei/pandax/GeneralResource.java
@@ -0,0 +1,19 @@
+package fr.efrei.pandax;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Default-case rest entry, useless except for verification purposes
+ */
+@Path("/")
+public class GeneralResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ping() {
+        return Response.ok("Pong !").build();
+    }
+}
