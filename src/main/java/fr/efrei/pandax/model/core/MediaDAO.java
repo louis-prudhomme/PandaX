2f83311ae2f6d2700a0dd0d70baf3073c3dea515
PrideTheDaemon <hellangel@live.fr>
2019-11-27 19:43:45 +0100

    Added route to get all medias for a media type Added route to get all medias for a publisher Added corresponding postman tests Refactored publisher and media type resources to use uribuilder

--- src/main/java/fr/efrei/pandax/model/core/MediaDAO.java
+++ src/main/java/fr/efrei/pandax/model/core/MediaDAO.java
@@ -1,11 +1,44 @@
 package fr.efrei.pandax.model.core;
 
 import fr.efrei.pandax.model.business.Media;
+import fr.efrei.pandax.model.business.MediaType;
+import fr.efrei.pandax.model.business.Publisher;
+
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Stateless
 public class MediaDAO extends AbstractDAO<Media> {   
     public MediaDAO(){
         super(Media.class);
+    }
+
+    /**
+     * Retrives every {@link Media} with specified {@link MediaType}
+     * @param idMedia desired {@link MediaType}
+     * @return an {@link List} of all matching {@link Media}
+     */
+    public List<Media> getAllForType(int idMedia) {
+        openEntityManager();
+        TypedQuery<Media> query = em.createNamedQuery("Media.findByMediaType", Media.class);
+        query.setParameter("mediaType", idMedia);
+        List<Media> media = query.getResultList();
+        closeEntityManager();
+        return media;
+    }
+
+    /**
+     * Retrives every {@link Media} with specified {@link Publisher}
+     * @param idPublisher desired {@link Publisher}
+     * @return an {@link List} of all matching {@link Media}
+     */
+    public List<Media> getAllForPublisher(int idPublisher) {
+        openEntityManager();
+        TypedQuery<Media> query = em.createNamedQuery("Media.findByPublisher", Media.class);
+        query.setParameter("publisher", idPublisher);
+        List<Media> mediaList = query.getResultList();
+        closeEntityManager();
+        return mediaList;
     }
 }
--- src/main/java/fr/efrei/pandax/resource/MediaTypeResource.java
+++ src/main/java/fr/efrei/pandax/resource/MediaTypeResource.java
@@ -1,13 +1,17 @@
 package fr.efrei.pandax.resource;
 
+import fr.efrei.pandax.model.business.Media;
 import fr.efrei.pandax.model.business.MediaType;
+import fr.efrei.pandax.model.core.MediaDAO;
 import fr.efrei.pandax.model.core.MediaTypeDAO;
 import fr.efrei.pandax.security.Role;
 import fr.efrei.pandax.security.Secured;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.*;
@@ -15,6 +19,9 @@ import static javax.ws.rs.core.MediaType.*;
 @Secured(Role.ADMIN)
 @Path("mediatype")
 public class MediaTypeResource {
+    @Context
+    UriInfo uriInfo;
+
     @GET
     @Secured({Role.USER, Role.ADMIN})
     @Produces(APPLICATION_JSON)
@@ -27,7 +34,12 @@ public class MediaTypeResource {
     @Consumes(APPLICATION_FORM_URLENCODED)
     public Response createOne(@FormParam("mediatype") MediaType type) {
         type = new MediaTypeDAO().create(type);
-        return Response.ok("/mediatype/" + type.getId()).build();
+        return Response
+                .ok(uriInfo.getBaseUriBuilder()
+                    .path(MediaTypeResource.class)
+                    .path(MediaTypeResource.class, "getOne")
+                    .build(type.getId()).toString())
+                .build();
     }
 
     @DELETE
@@ -36,7 +48,12 @@ public class MediaTypeResource {
     public Response deleteOne(@PathParam("id")int id) {
         MediaTypeDAO dao = new MediaTypeDAO();
         dao.delete(dao.read(id));
-        return Response.ok("/mediatype/").build();
+        return Response
+                .ok(uriInfo.getBaseUriBuilder()
+                    .path(MediaTypeResource.class)
+                    .path(MediaTypeResource.class, "getAll")
+                    .build().toString())
+                .build();
     }
 
     @GET
@@ -44,13 +61,34 @@ public class MediaTypeResource {
     @Secured({Role.USER, Role.ADMIN})
     @Produces(APPLICATION_JSON)
     public Response getOne(@PathParam("id")int id) {
-        return Response.ok(new MediaTypeDAO().read(id)).build();
+        return Response.ok(
+                new MediaTypeDAO().read(id))
+                .build();
     }
 
     @PUT
     @Consumes(APPLICATION_FORM_URLENCODED)
     public Response updateOne(@FormParam("mediatype")MediaType type) {
         type = new MediaTypeDAO().modify(type);
-        return Response.ok("/mediatype/" + type.getId()).build();
+        return Response
+                .ok(uriInfo.getBaseUriBuilder()
+                        .path(MediaTypeResource.class)
+                        .path(MediaTypeResource.class, "getOne")
+                        .build(type.getId()).toString())
+                .build();
+    }
+
+    /**
+     * Gets all {@link Media} for a specified {@link MediaType}
+     * @param idMedia of the desired {@link MediaType}
+     * @return an {@link List} of all matching {@link Media}
+     */
+    @GET
+    @Path("/{idMediaType}/media")
+    public Response getMediaForType(@PathParam("idMediaType")int idMedia) {
+        List<Media> media = new MediaDAO().getAllForType(idMedia);
+        return Response.ok(
+                new GenericEntity<>(media) {})
+                .build();
     }
 }
